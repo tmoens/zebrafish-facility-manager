a48d230280ea2111afa5a9fdd879214d8927d4fa
Ted Moens <ted.moens@gmail.com>
2021-05-04 11:21:24 -0700

    enhancement: added SQL script for initial population of data

---
+++ zf-server/mysql scripts/mutation_types.sql
@@ -0,0 +1,12 @@
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (1, 'Deletion', 'Del', 'a deletion of sequences between two induced double-strand breaks or by homologous recombination.');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (2, 'Enhancer Trap', 'Et', 'insertion of exogenously provided sequences outside the coding sequence of a gene, resulting in reporter expression.');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (4, 'InDel', 'InDel', 'a small insertion or deletion that changes the reading frame of the mRNA, typically induced at the site of a single targeted double-strand break.');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (5, 'Gene Trap', 'Gt', 'insertion of exogenously provided sequences inside the coding sequence of a gene, resulting in a fusion transcript.');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (6, 'Missense', 'Mis', 'a single nucleotide change that changes an amino acid in the coding sequence of a gene.');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (7, 'Nonsense', 'Stop', 'a single nucleotide change that creates a TAG, TAA or TGA codon in the coding sequence of a gene.');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (9, 'Splice site', 'Spl', 'a nucleotide change that disrupts the sequence of a splice donor or splice acceptor.');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (13, 'Random Insertion', 'Ins (ran)', 'non-targeted insertion of exogenously provided sequences causing a disruption of gene function');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (14, 'Targeted insertion', 'Ins (tar)', 'targeted insertion of a marker or loxP element into a gene using homology-mediated mechanisms');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (19, 'Point', 'Point', 'Please map to the specific type of point mutation');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (20, 'Spontaneous', 'sp', 'Spontaneous mutation');
+INSERT INTO mutation_type (id, name, abbrv, description) VALUES (21, 'Insertion', 'Ins', 'Please change to Random Insertion or Targeted Insertion.');
---
+++ zf-server/mysql scripts/screen_types.sql
@@ -0,0 +1,8 @@
+INSERT INTO screen_type (id, name, description) VALUES (1, 'CRISPR', 'made using CRISPR without added DNA or oligos');
+INSERT INTO screen_type (id, name, description) VALUES (2, 'CRISPR (HDR)', 'made using homology-directed repair or homologous recombination');
+INSERT INTO screen_type (id, name, description) VALUES (3, 'CRISPR (non-HR)', 'CRISPR-mediated insertion of DNA without homology to target sequence. E.g. Higashijima-style insertion.');
+INSERT INTO screen_type (id, name, description) VALUES (4, 'ENU', 'identified in a forward genetic screen of chemically-mutagenized fish');
+INSERT INTO screen_type (id, name, description) VALUES (5, 'Insertional Mutagenesis', 'made by the random insertion of a DNA element (e.g. retrovirus or transposon) that acts as a mutagen rather than a reporter.');
+INSERT INTO screen_type (id, name, description) VALUES (6, 'TALEN', 'made using TALENs');
+INSERT INTO screen_type (id, name, description) VALUES (7, 'TILLING', 'identified by TILLING of ENU-mutagenized fish');
+INSERT INTO screen_type (id, name, description) VALUES (8, 'CRISPR Pair', 'TBD');
